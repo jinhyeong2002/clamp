f78c21687290c64649ddcca376ae7c445e48dfa3
Lee jinhyeong <leejinhyeong@Lees-MacBook-Air.local>
2025-12-22 00:05:28 +0900

    Fix User entity mapping and enable auto-generated ID

--- src/main/java/com/example/cltmp/model/User.java
+++ src/main/java/com/example/cltmp/model/User.java
@@ -1,12 +1,10 @@
 package com.example.cltmp.model;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
+import jakarta.persistence.*;
 import lombok.Data;
 
 @Entity
+@Table(name = "users")
 @Data
 public class User {
 
